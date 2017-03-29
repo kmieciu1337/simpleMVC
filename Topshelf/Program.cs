21503335293f4d39a578fb5d6e47b126e8b572dc
kmieciu1337 <pkmiecik2105@gmail.com>
2017-03-29 18:40:26 +0200

    Added Topshelf project

---
+++ Topshelf/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Logging;
+
+namespace Topshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.UseLog4Net();
+                x.Service<SimpleService>(s =>
+                {
+                    s.ConstructUsing(() => new SimpleService());
+                    s.WhenStarted(start => start.Start());
+                    s.WhenStopped(stop => stop.Stop());
+                });
+            }
+            );
+        }
+    }
+    class SimpleService
+    {
+        static LogWriter _logger = HostLogger.Get<SimpleService>();
+        public SimpleService()
+        {
+            
+        }
+        public void Start()
+        {
+            _logger.Info($"Starting {this.GetType().Name} service");
+            Console.WriteLine("Service is running");
+            System.Threading.Thread.Sleep(1000);
+            _logger.Info($"Service {this.GetType().Name} started");
+        }
+        public void Stop()
+        {
+            _logger.Info($"Stopping {this.GetType().Name} service");
+            Console.WriteLine("Service is stopping");
+            System.Threading.Thread.Sleep(1000);
+            _logger.Info($"Service {this.GetType().Name} stopped");
+        }
+    }
+}
